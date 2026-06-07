5257987ed45ae153f8ab11e8907b4dcf86d8541c
HannaEpelboim <h4nn42005@gmail.com>
2026-06-07 19:55:58 -0300

    rpg deszipado e refatorado

---
+++ rpg_mermo/rpg_mermo/scripts/vivi.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+var main_dialogue = preload("res://dialog_vivi.dialogue")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	DialogueManager.show_dialogue_balloon(main_dialogue)
